b69457c2a86bb945fbed37bea80f930d3b059a7d
VishnuReddy <vishnureddy.au07@gmail.com>
2026-03-20 23:50:51 +0530

    fix: allow PATCH in CORS preflight for SOS close-case endpoint

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
   if (allowed) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
     console.log('Handling OPTIONS');
